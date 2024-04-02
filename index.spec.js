45e20da144ad45e2a053674df024f6dfe9037316
Guilherme de Almeida Pastl <guilherme.pastl@seventh.com.br>
2024-04-02 08:50:18 -0300

    Add another failling test

--- index.spec.js
+++ index.spec.js
@@ -6,3 +6,14 @@ it('se a lista estiver vazia, a comissão é zero', () => {
 
     expect(resultadoAtual).toBe(resultadoEsperado)
 })
+
+it('calcula comissão quando só tem um item na lista', () => {
+    const resultadoAtual = calculaComissaoDeVenda([{
+        id: 'PROD-123',
+        preco: 1000,
+        quantidadeVEndida: 1
+    }])
+    const resultadoEsperado = 50
+
+    expect(resultadoAtual).toBe(resultadoEsperado)
+})
