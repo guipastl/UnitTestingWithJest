1af80909d2b1738b9df32062fc435dc51359757b
Guilherme de Almeida Pastl <guilherme.pastl@seventh.com.br>
2024-04-02 09:13:41 -0300

    Make all tests pass again

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 module.exports = function(itens) {
     if (!itens.length) return 0
-    const { precoUnitario, quantidadeVendida } = itens[0]
-    return precoUnitario * quantidadeVendida * .05
+    if (itens.length === 1) {
+        const { precoUnitario, quantidadeVendida } = itens[0]
+        return precoUnitario * quantidadeVendida * .05
+    }
+    if (itens.length > 1) {
+        const prices = itens.map(item => {
+            const { precoUnitario, quantidadeVendida } = item
+            return precoUnitario * quantidadeVendida
+        })
+
+        const totalPrice = prices.reduce((a, b) => a + b)
+
+        return totalPrice * .05
+    }
 }
