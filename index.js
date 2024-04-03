d5f9330bbb304d60531255c787c361576f90df0d
Guilherme de Almeida Pastl <guilherme.pastl@seventh.com.br>
2024-04-03 08:18:47 -0300

    Refector a bit more

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 module.exports = function(itens) {
     if (!itens.length) return 0
-    const prices = itens.map(item => {
+    const totalPrice = itens.map(item => {
         const { precoUnitario, quantidadeVendida } = item
         return precoUnitario * quantidadeVendida
-    })
-
-    const totalPrice = prices.reduce((a, b) => a + b)
+    }).reduce((a, b) => a + b)
 
     return totalPrice * .05
 }
