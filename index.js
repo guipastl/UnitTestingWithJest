9439518eae8367ac7b92b229651ce5d0e868bad5
Guilherme de Almeida Pastl <guilherme.pastl@seventh.com.br>
2024-04-03 09:09:14 -0300

    Remove duplicated code by adding multiply function

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ module.exports = function(itens) {
     const totalPrice = itens.map(item => {
         const { id, precoUnitario, quantidadeVendida } = item
         if (id === 'XP-0101') foundSpecialProduct = true
-        return precoUnitario * quantidadeVendida
+        return multiply(precoUnitario, quantidadeVendida)
     }).reduce((a, b) => a + b, 0)
-    if (foundSpecialProduct) return totalPrice * .2
-    if (totalPrice < 1999.99) return totalPrice * .05
-    if (totalPrice > 1999.99 && totalPrice < 4999.99) return totalPrice * .1
-    if (totalPrice > 4999.99) return totalPrice * .15
+    if (foundSpecialProduct) return multiply(totalPrice, .2)
+    if (totalPrice < 1999.99) return multiply(totalPrice, .05)
+    if (totalPrice > 1999.99 && totalPrice < 4999.99) return multiply(totalPrice, .1)
+    if (totalPrice > 4999.99) return multiply(totalPrice, .15)
+}
+
+// Função acessória que não exige um teste para si
+function multiply(val1, val2) {
+    return val1 * val2
 }
