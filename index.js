a9d479f358ab8dcff856f87668841aea32329b6e
Guilherme de Almeida Pastl <guilherme.pastl@seventh.com.br>
2024-04-03 08:17:04 -0300

    Refactor code a bit

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 module.exports = function(itens) {
     if (!itens.length) return 0
-    if (itens.length === 1) {
-        const { precoUnitario, quantidadeVendida } = itens[0]
-        return precoUnitario * quantidadeVendida * .05
-    }
-    if (itens.length > 1) {
-        const prices = itens.map(item => {
-            const { precoUnitario, quantidadeVendida } = item
-            return precoUnitario * quantidadeVendida
-        })
+    const prices = itens.map(item => {
+        const { precoUnitario, quantidadeVendida } = item
+        return precoUnitario * quantidadeVendida
+    })
 
-        const totalPrice = prices.reduce((a, b) => a + b)
+    const totalPrice = prices.reduce((a, b) => a + b)
 
-        return totalPrice * .05
-    }
+    return totalPrice * .05
 }
