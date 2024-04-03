9b3dacfa0cf384302b80ada29418b8013ffebd79
Guilherme de Almeida Pastl <guilherme.pastl@seventh.com.br>
2024-04-03 08:56:13 -0300

    Add special comission feature

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 module.exports = function(itens) {
+    let foundSpecialProduct = false
     const totalPrice = itens.map(item => {
-        const { precoUnitario, quantidadeVendida } = item
+        const { id, precoUnitario, quantidadeVendida } = item
+        if (id === 'XP-0101') {
+            foundSpecialProduct = true
+        }
         return precoUnitario * quantidadeVendida
     }).reduce((a, b) => a + b, 0)
+    if (foundSpecialProduct) return totalPrice * .2
     if (totalPrice < 1999.99) return totalPrice * .05
     if (totalPrice > 1999.99 && totalPrice < 4999.99) return totalPrice * .1
     if (totalPrice > 4999.99) return totalPrice * .15
